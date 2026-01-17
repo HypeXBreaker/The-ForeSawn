874f3b4a616b951e6753697852987f48a2c9addc
HypeXBreaker <adityamohapatraofficial@gmail.com>
2026-01-17 11:35:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ const buttonInfo = {
     "DrewMatica": "Visit DrewMatica website. AI Study Tutor (TESTINGs PHRASE).",
     "PromeTheus": "Visit PromeTheus. AI-powered PROMPT generator for various purposes.",
     "Drastica": "Explore Drastica. AI-powered chat interface.",
-    "OtherBots": "Older Bots ever made. Past insights reflected.",
+    "BOTs": "Older Bots ever made. Past insights reflected.",
     "Ascension": "Witness Ascension. Advanced stages of foresight."
 };
 
